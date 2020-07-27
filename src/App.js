81e1082ab1674af558062278dc773dd3f14b5e66
Marius Andra <marius.andra@gmail.com>
2020-07-27 15:38:14 +0200

    use posthog-js npm module, load as late as possible, add buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import logo from "./logo.svg";
+import "./App.css";
+import posthog from 'posthog-js'
 
 function App() {
+  // This loads posthog as late as possible - after React finished rendering
+  useEffect(() => {
+    posthog.init("8jVz0YZ2YPtP7eL1I5l5RQIp-WcuFeD3pZO8c0YDMx4", {
+      api_host: "http://localhost:8000",
+    });
+    window.posthog = posthog;
+
+    posthog.identify('testUser')
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +21,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button>Do something</button>
+        <button>Yes</button>
+        <button>Hola!</button>
+        <button>Hey there</button>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+// Uncomment this to init before rendering
+
+// import posthog from "posthog-js";
+// posthog.init("8jVz0YZ2YPtP7eL1I5l5RQIp-WcuFeD3pZO8c0YDMx4", {
+//   api_host: "http://localhost:8000",
+// });
+// window.posthog = posthog;
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
